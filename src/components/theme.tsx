9fc39ac22b5c5b89953668d152bf5638d554be81
Ouvill <contact@ouvill.net>
2019-06-13 01:22:33 +0900

    update Bio

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,4 +1,10 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles"
 
-const default_theme = createMuiTheme({})
+const default_theme = createMuiTheme({
+  palette: {
+    text: {
+      primary: "#333333",
+    },
+  },
+})
 export const theme = responsiveFontSizes(default_theme)
