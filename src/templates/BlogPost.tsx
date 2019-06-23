f6751dd8e90e22a46d5477890e3a3cbc0e638caf
Ouvill <contact@ouvill.net>
2019-06-23 22:30:16 +0900

    change date format

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -286,7 +286,7 @@ export const pageQuery = graphql`
       tableOfContents
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY/MM/DD,")
         description
       }
     }
