6afb91068d64fd72c1e446d32dd7d846f6e76881
Ouvill <contact@ouvill.net>
2023-01-05 17:09:47 +0000

    refactor(gatsby-node): split file

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,7 @@
 import { GatsbyNode } from "gatsby"
 import { createFilePath } from "gatsby-source-filesystem"
-import path from "path"
 import webpack from "webpack"
+export { createPages } from "./src/gatsbyjs/gatsby-node/createPages"
 
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
   loaders,
@@ -29,17 +29,6 @@ export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] 
     frontmatter: MarkdownRemarkFrontmatter
   }`)
   }
-
-export const createPages: GatsbyNode["createPages"] = async ({
-  graphql,
-  actions,
-}) => {
-  const { createPage } = actions
-
-  await createBlogPosts(graphql, createPage)
-  await createStaticPages(graphql, createPage)
-  await createIndexPage(graphql, createPage)
-}
 
 export const onCreateNode: GatsbyNode["onCreateNode"] = ({
   node,
@@ -76,153 +65,4 @@ export const onCreateNode: GatsbyNode["onCreateNode"] = ({
       })
     }
   }
-}
-//
-function createIndexPage(graphql, createPage) {
-  const indexPage = path.resolve("./src/templates/indexes.tsx")
-  const listNum = 12
-  //
-  return graphql(`
-    {
-      site {
-        siteMetadata {
-          defaultCover
-        }
-      }
-      allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/content/blog/" } }
-      ) {
-        totalCount
-      }
-    }
-  `).then((result) => {
-    if (result.errors) {
-      throw result.errors
-    }
-    //
-    const total = result.data.allMarkdownRemark.totalCount
-    const defaultCover = result.data.site.siteMetadata.defaultCover
-    const pages = total / listNum
-    const rest = total % listNum
-    for (let i = 0; i < pages; i++) {
-      const next = (i === pages - 1 && rest) || i < pages - 1 ? i + 1 : null
-      const previous = i === 0 ? null : i - 1
-      if (i == 0) {
-        createPage({
-          path: "/",
-          component: indexPage,
-          context: {
-            index: i * listNum,
-            defaultCover,
-            previous,
-            next,
-          },
-        })
-      }
-      //
-      createPage({
-        path: `indexes/${i}`,
-        component: indexPage,
-        context: {
-          index: i * listNum,
-          defaultCover,
-          previous,
-          next,
-        },
-      })
-    }
-  })
-}
-//
-function createStaticPages(graphql, createPage) {
-  const blogPost = path.resolve(`./src/templates/StaticPage.tsx`)
-  return graphql(`
-    {
-      allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/content/pages/" } }
-        sort: { frontmatter: { date: DESC } }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
-          }
-        }
-      }
-    }
-  `).then((result) => {
-    if (result.errors) {
-      console.log(result.errors)
-      throw result.errors
-    }
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
-    posts.forEach((post, index) => {
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-        },
-      })
-    })
-    return null
-  })
-}
-//
-function createBlogPosts(graphql, createPage) {
-  const blogPost = path.resolve(`./src/templates/BlogPost.tsx`)
-  return graphql(`
-    {
-      site {
-        siteMetadata {
-          defaultCover
-        }
-      }
-      allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/content/blog/" } }
-        sort: { frontmatter: { date: DESC } }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
-          }
-        }
-      }
-    }
-  `).then((result) => {
-    if (result.errors) {
-      throw result.errors
-    }
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
-    const defaultCover = result.data.site.siteMetadata.defaultCover
-    //
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          defaultCover,
-          previous,
-          next,
-        },
-      })
-    })
-    return null
-  })
 }
